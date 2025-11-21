af8da238ae677eaf4e3f1b753e5a4bf24a9f827d
RafUD <rafaelseriousd@gmail.com>
2025-11-20 20:01:46 -0500

    Continué à implémenter Cameras, Terrain, Eau

---
+++ Assets/Scripts/IntroCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+public class IntroCamera : MonoBehaviour
+{
+    public float delayInSeconds = 5f; // Set the delay duration in the Inspector
+
+    void Start()
+    {
+        // Start the coroutine when the script begins
+        StartCoroutine(DeactivateObjectAfterDelay());
+    }
+
+    IEnumerator DeactivateObjectAfterDelay()
+    {
+        // Wait for the specified duration
+        yield return new WaitForSeconds(delayInSeconds);
+
+        // Deactivate the GameObject this script is attached to
+        gameObject.SetActive(false);
+    }
+}
